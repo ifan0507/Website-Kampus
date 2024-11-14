5e29ff9722968129f5352a362217f33abab86d2c
faisalandriansah <faisalrahmad039@gmail.com>
2024-11-14 14:09:49 +0700

    update view berita

--- frontend/src/views/question/forms/add-question-form.jsx
+++ frontend/src/views/question/forms/add-question-form.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Modal, Form, Upload, Icon, Input } from 'antd';
+import { Modal, Form, Upload, Icon, Input, Select} from 'antd';
 const { TextArea } = Input;
+const { Option } = Select;
 
 
 class AddQuestionForm extends Component {
   state = {
     selectedFile: null,
+    categories: [],
+    loading: false,
   };
+
+  BASE_URL = 'http://localhost:8080';
+
+  fetchCategories = () => {
+    this.setState({ loading: true });
+    axios
+      .get(`${this.BASE_URL}/api/category-berita`)
+      .then((response) => {
+        console.log("Kategori Berita Diterima", response.data.content);
+        this.setState({ categories: response.data.content, loading: false });
+      })
+      .catch((error) => {
+        console.error('Error fetching categories:', error);
+        this.setState({ loading: false });
+      });
+  };
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
 
   fileSelectedHandler = (event) => {
     this.setState({
@@ -32,6 +56,7 @@ class AddQuestionForm extends Component {
   render() {
     const { visible, onCancel, onOk, form, confirmLoading } = this.props;
     const { getFieldDecorator } = form;
+    const { categories, loading} = this.state;
 
     const formItemLayout = {
       labelCol: {
@@ -43,7 +68,9 @@ class AddQuestionForm extends Component {
         sm: { span: 16 },
       },
     };
-
+ 
+  
+   
     return (
       <Modal
         title="Tambah Berita"
@@ -78,6 +105,39 @@ class AddQuestionForm extends Component {
               ],
             })(<TextArea rows={4} placeholder="Judul" />)}
           </Form.Item>
+
+          <Form.Item label="Category Berita:">
+           {getFieldDecorator('category-berita', {
+            rules: [{ required: true, message: 'Silahkan pilih kategori berita' }],
+            })(
+            <Select placeholder="Pilih Kategori Berita" loading={loading}>
+            {categories.length > 0 ? (
+            categories.map((category) => (
+            <Option key={category.id} value={category.id}>
+            {category.name}
+          </Option>
+        ))
+      ) : (
+        <Option disabled value="">
+          Tidak ada kategori
+        </Option>
+      )}
+    </Select>
+  )}
+</Form.Item>
+          
+          <Form.Item label="Galeri Berita:">
+            {getFieldDecorator("galeri-baru", {
+              rules: [{ required: true, message: "Silahkan pilih galeri berita" }],
+            })(
+              <Select placeholder="Pilih Kategori Galeri Berita">
+                <Option value="nasional">Nasional</Option>
+                <Option value="internasional">Internasional</Option>
+                <Option value="politik">Politik</Option>
+              </Select>
+            )}
+          </Form.Item>
+          
           <Form.Item label="Deskripsi:">
             {getFieldDecorator("description", {
               rules: [
@@ -88,6 +148,7 @@ class AddQuestionForm extends Component {
               ],
             })(<TextArea rows={4} placeholder="Deskripsi" />)}
           </Form.Item>
+
           <Form.Item label="Selengkapnya:">
             {getFieldDecorator("selengkapnya", {
               rules: [
